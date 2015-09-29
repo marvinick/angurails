dea92949adc0c44b6d4273c442910a50521e6670
marvinick <marviluw@gmail.com>
2015-09-28 19:44:12 -0700

    commit

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,2 +1,14 @@
 class Visitor < ActiveRecord::Base
+
+  validates_attachment_content_type :image,
+  content_type:  /^image\/(png|gif|jpeg)/,
+  message: "Only images allowed"
+
+  validates_attachment_content_type :resource,
+  content_type: ['application/pdf'],
+  message: "Only pdfs allowed"
+
+  validates :image, attachment_presence: true
+  validates :resource, attachment_presence: true
+
 end
