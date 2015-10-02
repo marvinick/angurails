2bb14a774ea566fc5791d3f8b20eb5885d4352a8
marvin <marvkipi@gmail.com>
2015-10-01 20:19:04 -0700

    image broken

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,6 +4,10 @@ class Picture < ActiveRecord::Base
 
   after_save :enqueue_image
 
+  def image_name
+    File.basename(image.path || image.filename) if image
+  end
+
   def enqueue_image
     ImageWorker.perform_async(id, key) if key.present?
   end
@@ -16,6 +20,7 @@ class Picture < ActiveRecord::Base
       picture.key = key
       picture.remote_image_url = picture.image.direct_fog_url(with_path: true)
       picture.save!
+      picture.update_column(:image_processed, true)
     end
   end
 end
