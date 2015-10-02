9d57ed37370a9d2d6e77ded0dcfdee196d5ac76d
marvin <marvkipi@gmail.com>
2015-10-01 19:56:10 -0700

    after queue

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,6 +2,8 @@ class Picture < ActiveRecord::Base
 
   mount_uploader :image, ImageUploader
 
+  after_save :enqueue_image
+
   def image_name
     File.basename(image.path || image.filename) if image
   end
