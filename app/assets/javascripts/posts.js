744bc89e220d53c5f806078596b86a22d6b27dee
marvin <marvkipi@gmail.com>
2015-10-07 13:11:28 -0700

    posts angular module

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,3 +1,30 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+var post = angular.module('Post', ['ngResource']);
+
+post.factory("Post", function($resource) {
+  return $resource("posts/:id", { id: '@id' }, {
+    index:   { method: 'GET', isArray: true, responseType: 'json' },
+    update:  { method: 'PUT', responseType: 'json' }
+  });
+})
+
+post.controller("PostsController", function($scope, Post) {
+  $scope.Posts = Post.index()
+
+  $scope.addPost = function() {
+    post = Post.save($scope.newPost)
+
+    $scope.posts.push(post)
+    $scope.newVisitor = {}
+  }
+
+  $scope.deletePost = function(index) {
+
+    post = $scope.posts[index]
+    Post.delete(post)
+    $scope.posts.splice(index, 1);
+  }
+})
