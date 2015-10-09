6acc98a613ad9baeb141855e6ddf20a890553c9a
marvin <marvkipi@gmail.com>
2015-10-08 17:07:22 -0700

    sign_out link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+
   resources :visitors, only: [:index, :create, :destroy], defaults: {format: :json}
   root to: "visitors#index"
 
