7144c1d6e1d8a9c4b1c41c1a1aae6fbad1938164
marvin <marvkipi@gmail.com>
2015-10-09 11:46:20 -0700

    user_id association to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user_id = current_user.id if current_user
     @post.save
     redirect_to posts_path
   end
