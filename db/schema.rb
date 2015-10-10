32e574799c565d39ecfe98979632d12f27ca4166
marvin <marvkipi@gmail.com>
2015-10-09 19:17:05 -0700

    location migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151009183656) do
+ActiveRecord::Schema.define(version: 20151010021430) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "media", force: :cascade do |t|
     t.string   "file_name"
