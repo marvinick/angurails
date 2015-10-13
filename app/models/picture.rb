46b1ccb3e6e53730b6ef500d0373bc861d4e08f1
marvin <marvkipi@gmail.com>
2015-10-12 21:19:00 -0700

    generate_slug on picture

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,4 +4,15 @@ class Picture < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   mount_uploader :image, ImageUploader
+
+  before_save :generate_slug
+
+  def to_param
+    self.slug
+  end
+
+  def generate_slug
+    self.slug = self.title.gsub(" ", "-")
+  end
+
 end
